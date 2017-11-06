14a577140c17509be816d48ab702fbaa9bae34f2
hoang7020 <hoang7020@gmail.com>
2017-11-06 11:13:22 +0700

    hoang da sua

--- DemoLamtt/src/demolamtt/DemoLamtt.java
+++ DemoLamtt/src/demolamtt/DemoLamtt.java
@@ -17,6 +17,7 @@ public class DemoLamtt {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Lam :))");
+        System.out.println("Hoang dep trai");
     }
     
 }
