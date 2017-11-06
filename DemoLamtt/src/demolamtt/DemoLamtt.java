9a5267447637dfc035153ab85355b3c6770d9fb4
Lâm Trần Thanh <royalloverubi@gmail.com>
2017-11-06 11:10:13 +0700

    Lam up

---
+++ DemoLamtt/src/demolamtt/DemoLamtt.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demolamtt;
+
+/**
+ *
+ * @author royal
+ */
+public class DemoLamtt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Lam :))");
+    }
+    
+}
